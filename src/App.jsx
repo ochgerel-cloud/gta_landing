3779a9eff02290027494a124100f3db9ccc9ad62
ochgerel-cloud <ochgerel999@gmail.com>
2025-11-04 20:20:27 +0800

    Hero section .mask-wrapper scroll хийхэд animate хийдэг боллоо

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
+import Hero from "./sections/Hero";
+import NavBar from "./sections/NavBar";
 
 gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
     <main>
-      <div>
-        <h1>Welcome to the world of GTA and GSAP!</h1>
-      </div>
+      <NavBar />
+      <Hero />
     </main>
   );
 };
